771c904f03e5172c2efa0a3626b3aeadba88f6ac
brunorenz <65bruno@gmail.com>
2020-03-10 16:03:45 +0100

    config

--- src/services/config.js
+++ src/services/config.js
@@ -1,11 +1,11 @@
 import { CustomTooltips } from "@coreui/coreui-plugin-chartjs-custom-tooltips";
 
-const local = true;
-const remote = false;
+const local = false;
+const remote = true;
 const serverUrlP = "/therm/rest/";
 const serverUrlR = "http://srvwas1.bpbari.it:8099/rest/";
 const serverUrlRemote = remote
-  ? "https://65bruno.ddns.net/therm/rest/"
+  ? "https://therm.65bruno.it/therm/rest/"
   : "http://192.168.0.101:8101/therm/rest/";
 const serverUrlD = local
   ? "http://localhost:8101/therm/rest/"
